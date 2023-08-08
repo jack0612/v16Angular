d8cb3015ed8ad32847e023be9934df5e93b95266
jack.lu <jack.lu@innovapost.com>
2023-08-07 21:41:06 -0400

    bar

--- src/app/my-echart/graph-simple/graph-simple.component.ts
+++ src/app/my-echart/graph-simple/graph-simple.component.ts
@@ -1,4 +1,4 @@
- 
+
 
 import { Component } from '@angular/core';
 import type { EChartsOption } from 'echarts';
@@ -8,95 +8,137 @@ import type { EChartsOption } from 'echarts';
   templateUrl: './graph-simple.component.html',
 })
 export class GraphSimpleComponent {
+
+  seriesLabel = {
+    normal: {
+      show: true,
+      textBorderColor: '#333',
+      textBorderWidth: 2
+    }
+  }
+  data = [
+    {
+      name: 'Solicitados',
+      value: 18203,
+    },
+    {
+      name: 'Aprobados',
+      value: 12000,
+    },
+    {
+      name: 'Aceptados',
+      value: 13203,
+    },
+    {
+      name: 'Cerrados',
+      value: 8203,
+    },
+    {
+      name: 'Negados',
+      value: 1203,
+    },
+    {
+      name: 'En revision',
+      value: 6203,
+    },
+    {
+      name: 'En mora',
+      value: 2203,
+    },
+  ];
+  colors = {
+    0: '#00000',
+    1: '#eeeee',
+    2: '#ccc',
+    3: '#bcbcbc',
+    4: '#eeffee',
+    5: '#ff0000',
+    6: '#fff000',
+  };
   options: EChartsOption = {
     title: {
-      text: 'Simple Graph',
+      text: 'World Population',
     },
-    tooltip: {},
-    animationDurationUpdate: 1500,
-    animationEasingUpdate: 'quinticInOut',
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    xAxis: {
+      type: 'value',
+      boundaryGap: [0, 0.01],
+    },
+    yAxis: {
+      type: 'category',
+      data: this.data.map((item) => item.name).reverse(),
+    },
     series: [
       {
-        type: 'graph',
-        layout: 'none',
-        symbolSize: 60,
-        roam: true,
+        type: 'bar',
+        data: this.data.map((item) => item.value).reverse(),
+        // itemStyle: {
+        //   color: function (param) {
+        //     console.log(param);
+        //     return this.colors[param.dataIndex];
+        //   },
+        // },
         label: {
-          show: true,
+          position: 'top'
         },
-        edgeSymbol: ['circle', 'arrow'],
-        edgeSymbolSize: [4, 10],
-        edgeLabel: {
-          fontSize: 20,
-        },
-        data: [
-          {
-            name: 'Node 1',
-            x: 300,
-            y: 300,
-          },
-          {
-            name: 'Node 2',
-            x: 800,
-            y: 300,
-          },
-          {
-            name: 'Node 3',
-            x: 550,
-            y: 100,
-          },
-          {
-            name: 'Node 4',
-            x: 550,
-            y: 500,
-          },
-        ],
-        // links: [],
-        links: [
-          {
-            source: 0,
-            target: 1,
-            symbolSize: [5, 20],
-            label: {
-              show: true,
-            },
-            lineStyle: {
-              width: 5,
-              curveness: 0.2,
-            },
-          },
-          {
-            source: 'Node 2',
-            target: 'Node 1',
-            label: {
-              show: true,
-            },
-            lineStyle: {
-              curveness: 0.2,
-            },
-          },
-          {
-            source: 'Node 1',
-            target: 'Node 3',
-          },
-          {
-            source: 'Node 2',
-            target: 'Node 3',
-          },
-          {
-            source: 'Node 2',
-            target: 'Node 4',
-          },
-          {
-            source: 'Node 1',
-            target: 'Node 4',
-          },
-        ],
-        lineStyle: {
-          opacity: 0.9,
-          width: 2,
-          curveness: 0,
-        },
+        // markPoint: {
+        //   symbolSize: 1,
+        //   symbolOffset: [0, '50%'],
+          // label: {
+          //    normal: {
+          //         formatter: '{a|{a}\n}{b|{b} }{c|{c}}',
+          //         backgroundColor: 'rgb(242,242,242)',
+          //         borderColor: '#aaa',
+          //         borderWidth: 1,
+          //         borderRadius: 4,
+          //         padding: [4, 10],
+          //         lineHeight: 26,
+          //         // shadowBlur: 5,
+          //         // shadowColor: '#000',
+          //         // shadowOffsetX: 0,
+          //         // shadowOffsetY: 1,
+          //         position: 'right',
+          //         distance: 20,
+          //         rich: {
+          //             a: {
+          //                 align: 'center',
+          //                 color: '#fff',
+          //                 fontSize: 18,
+          //                 textShadowBlur: 2,
+          //                 textShadowColor: '#000',
+          //                 textShadowOffsetX: 0,
+          //                 textShadowOffsetY: 1,
+          //                 textBorderColor: '#333',
+          //                 textBorderWidth: 2
+          //             },
+          //             b: {
+          //                  color: '#333'
+          //             },
+          //             c: {
+          //                 color: '#ff8811',
+          //                 textBorderColor: '#000',
+          //                 textBorderWidth: 1,
+          //                 fontSize: 22
+          //             }
+          //         }
+          //    }
+          // },
+          // data: [
+          //     {type: 'max', name: 'max days: '},
+          //     {type: 'min', name: 'min days: '}
+          // ]
+        // }
       },
     ],
   };
--- src/app/my-echart/line-chart/line-chart.component.ts
+++ src/app/my-echart/line-chart/line-chart.component.ts
@@ -16,13 +16,17 @@ export class LineChartComponent {
 
 
   chartOption: EChartsOption = {
+    textStyle: {
+      fontFamily: 'Roboto, Arial, Verdana, sans-serif',
+      fontSize: 24
+    },
     // showGridLines:true,
     xAxis: {
       type: 'category',
       boundaryGap: false,
       data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       splitLine: {
-        show: true
+        show: false,//true
       }
     },
     yAxis: {
@@ -31,25 +35,88 @@ export class LineChartComponent {
         show: false
       }
     },
-    tooltip: {
-      trigger: 'item',
-      showDelay: 0,
-      transitionDuration: 0.2,
-      formatter: function (params) {
-        console.log('line-chart:params', params)
-        return `<b>${params['name']}</b> : $ ${params['value']}`;
+    tooltip: [
+      {
+        //position :['right'],
+        trigger: 'item',
+        showDelay: 0,
+        transitionDuration: 0.2,
+        formatter: function (params) {
+          console.log('line-chart:params', params)
+          return `
+          <b>${params['name']}</b> : $ ${params['value']}
+        <div><b>${params['name']}</b> : $ ${params['value']}</div>
+        <div><b>${params['name']}</b> : $ ${params['value']}</div>
+        <div><b>${params['name']}</b> : $ ${params['value']}</div>
+        `;
+        },
+        textStyle: {
+          fontSize: 13,
+          fontFamily: 'Roboto, sans-serif'
+        }
+        ,
+        axisPointer: {
+          mainType: 'axisPointer',
+          type: 'line',
+        }
+      },
+      {
+        trigger: 'item',
+        showDelay: 0,
+        transitionDuration: 0.2,
+        formatter: function (params) {
+          console.log('line-chart:params', params)
+          return `aaaaaaaaaaaaa
+        `;
+        },
+        textStyle: {
+          fontSize: 13,
+          fontFamily: 'Roboto, sans-serif'
+        }
+        ,
+        axisPointer: {
+          mainType: 'axisPointer',
+          type: 'line',
+        }
       }
-    },
-    series: [{
-      data: [820, 932, 901, 934, 1290, 1430, 1550, 1200, 1650.1450, 1680, 1890, 2300],
-      type: 'line',
-      areaStyle: {}
-    },
-    {
-      data: [900, 2000],
-      type: 'line',
-      areaStyle: {}
-    },
+    ],
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1430, 1550, 1200, 1650.1450, 1680, 1890, 2300],
+        type: 'line',
+        areaStyle: {},
+        //https://github.com/apache/echarts/issues/11059 
+        //https://stackoverflow.com/questions/64781894/echarts-how-to-set-different-symbol-for-different-marklines-in-one-chart
+        markLine: {
+          symbol: 'none',
+          lineStyle: {
+            width: 1,
+            type: 'solid'
+          },
+          data: [
+            {
+              name: "test", xAxis: 'Feb',
+              label: {
+                position: 'insideEndTop',
+                formatter: '{b}:{c}',
+                show: false
+              }
+            },
+            {
+              name: "test2", xAxis: 'Mar',
+              label: {
+                show: false
+              }
+            }
+          ]
+        },
+
+      },
+      {
+        data: [900],
+        type: 'line',
+        areaStyle: {}
+      },
     ]
   }
 
